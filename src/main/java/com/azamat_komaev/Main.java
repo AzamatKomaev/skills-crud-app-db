b3e7c3f802850808ce4394d2983caedb1c46918c
Азамат Комаев <azamatkomaev15@gmail.com>
2023-02-06 20:06:03 +0300

    init commit

---
+++ src/main/java/com/azamat_komaev/Main.java
@@ -0,0 +1,7 @@
+package com.azamat_komaev;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ src/test/java/com/azamat_komaev/CalculatorTest.java
@@ -0,0 +1,16 @@
+package com.azamat_komaev;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CalculatorTest {
+
+    @Test
+    public void add() {
+    }
+
+    @Test
+    public void multiply() {
+    }
+}
