16805a4042510664dcf966454123bb2d150face8
Азамат Комаев <azamatkomaev15@gmail.com>
2023-02-11 00:13:20 +0300

    feature: add skill list for developers gotten from database

--- src/main/java/com/azamat_komaev/crudapp/repository/jdbc/JdbcDeveloperRepositoryImpl.java
+++ src/main/java/com/azamat_komaev/crudapp/repository/jdbc/JdbcDeveloperRepositoryImpl.java
@@ -6,6 +6,7 @@ import com.azamat_komaev.crudapp.model.Skill;
 import com.azamat_komaev.crudapp.model.Specialty;
 import com.azamat_komaev.crudapp.model.Status;
 import com.azamat_komaev.crudapp.repository.DeveloperRepository;
+import com.azamat_komaev.crudapp.repository.SkillRepository;
 
 import java.sql.Connection;
 import java.sql.ResultSet;
@@ -18,9 +19,8 @@ public class JdbcDeveloperRepositoryImpl implements DeveloperRepository {
     public JdbcDeveloperRepositoryImpl() {
     }
 
-    private Developer getDeveloperResultSet(ResultSet rs) throws SQLException {
+    private Developer getDeveloperResultSet(ResultSet rs, SkillRepository skillRepository) throws SQLException {
         int developerId = rs.getInt("d.id");
-
         String firstName = rs.getString("first_name");
         String lastName = rs.getString("last_name");
         Status isDeveloperActive = rs.getBoolean("d.active") ? Status.ACTIVE : Status.DELETED;
@@ -29,8 +29,16 @@ public class JdbcDeveloperRepositoryImpl implements DeveloperRepository {
         String specialtyName = rs.getString("sp.name");
         Status isSpecialtyActive = rs.getBoolean("sp.active") ? Status.ACTIVE : Status.DELETED;
 
+        String skillsId = rs.getString("skills_id");
+
         Specialty specialty = new Specialty(specialtyId, specialtyName, isSpecialtyActive);
         List<Skill> skillList = new ArrayList<>();
+
+        if (skillsId != null) {
+            for (String skillId: skillsId.split(" ")) {
+                skillList.add(skillRepository.getById(Integer.valueOf(skillId)));
+            }
+        }
 
         return new Developer(developerId, firstName, lastName,
                              isDeveloperActive, skillList, specialty);
@@ -43,11 +51,12 @@ public class JdbcDeveloperRepositoryImpl implements DeveloperRepository {
 
     @Override
     public List<Developer> getAll() {
-        String sqlQuery = "select d.*, sk.*, sp.* " +
+        String sqlQuery = "select d.*, sp.*, group_concat(sk.id separator ' ') as skills_id " +
                           "from developers d left join developers_skills ds on d.id = ds.developer_id " +
                           "left join skills sk on ds.skill_id = sk.id " +
-                          "left join specialties sp on d.specialty_id = sp.id";
+                          "left join specialties sp on d.specialty_id = sp.id group by d.id";
         List<Developer> developerList = new ArrayList<>();
+        SkillRepository skillRepository = new JdbcSkillRepositoryImpl();
 
         try (
             Connection conn = Database.getInstance().getConnection();
@@ -63,7 +72,7 @@ public class JdbcDeveloperRepositoryImpl implements DeveloperRepository {
                     continue;
                 }
 
-                Developer developer = getDeveloperResultSet(rs);
+                Developer developer = getDeveloperResultSet(rs, skillRepository);
                 developerMap.put(developerId, developer);
             }
 
